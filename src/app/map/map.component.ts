a929e13d6ec20c499a811492f259b014653c40cc
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2020-09-27 09:00:27 +0700

    loading data

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -29,27 +29,64 @@ export class MapComponent implements OnInit {
       center: [this.lng, this.lat]
       
     });
-    this.map.addControl(new mapboxgl.NavigationControl());
     this.map.on('load',  map => {
+      this.map.addControl(new mapboxgl.NavigationControl());
       this.map.setLayoutProperty('country-label', 'text-field', [
         'get',
         'name_ru'
         ]);
+        var marker = new mapboxgl.Marker()
+        .setLngLat([81, 54.8])
+        .addTo(this.map);
         const data = fireData;
-        console.log(data);
         data.forEach(bush => {
           var marker = new mapboxgl.Marker()
           .setLngLat([bush.xi, bush.yi])
           .addTo(this.map);
-          console.log(bush);
+        });
+        this.map.addSource("myImageSource", {
+          "type": "image",
+          "url": "../../assets/zem.png",
+          "coordinates": [
+              [74.88, 57.5],
+              [86.18, 57.5],
+              [86.18, 52.8],
+              [74.88, 52.8]
+            ]
+        });
+        this.map.addLayer({
+            "id": "overlay",
+            "source": "myImageSource",
+            "type": "raster",
+            "paint": {
+            "raster-opacity": 0.85
+            }
         });
     });
-    
   };
   
   select(event) {
     this.selectSubstrate = event.target.value;
     this.map.setStyle(this.style + this.selectSubstrate);
+    setTimeout(() =>{
+    this.map.addSource("myImageSource", {
+      "type": "image",
+      "url": "../../assets/zem.png",
+      "coordinates": [
+          [74.88, 57.5],
+          [86.18, 57.5],
+          [86.18, 52.8],
+          [74.88, 52.8]
+      ]
+    });
+    this.map.addLayer({
+        "id": "overlay",
+        "source": "myImageSource",
+        "type": "raster",
+        "paint": {
+        "raster-opacity": 0.85
+        }
+    });}, 1000)
   };
 
 }
