6f542151c3f2b7ed4f68a67c1aa607ed3e1180d8
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2020-11-28 20:49:08 +0700

    added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnChanges } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import fireData  from '../../assets/fire.json';
+import fireData from '../../assets/fire.json';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -16,9 +16,11 @@ export class MapComponent implements OnInit {
   style = 'mapbox://styles/mapbox/';
   lat = 55;
   lng = 82.8;
+  station = [];
+  markers: mapboxgl.Marker[] = [];
   selectSubstrate = 'streets-v11';
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
     this.map = new mapboxgl.Map({
@@ -27,66 +29,54 @@ export class MapComponent implements OnInit {
       style: this.style + this.selectSubstrate,
       zoom: 9,
       center: [this.lng, this.lat]
-      
+
     });
-    this.map.on('load',  map => {
+    this.map.on('load', map => {
       this.map.addControl(new mapboxgl.NavigationControl());
       this.map.setLayoutProperty('country-label', 'text-field', [
         'get',
         'name_ru'
-        ]);
-        var marker = new mapboxgl.Marker()
-        .setLngLat([81, 54.8])
-        .addTo(this.map);
-        const data = fireData;
-        data.forEach(bush => {
-          var marker = new mapboxgl.Marker()
-          .setLngLat([bush.xi, bush.yi])
-          .addTo(this.map);
-        });
-        this.map.addSource("myImageSource", {
-          "type": "image",
-          "url": "../../assets/zem.png",
-          "coordinates": [
-              [74.88, 57.5],
-              [86.18, 57.5],
-              [86.18, 52.8],
-              [74.88, 52.8]
-            ]
-        });
-        this.map.addLayer({
-            "id": "overlay",
-            "source": "myImageSource",
-            "type": "raster",
-            "paint": {
-            "raster-opacity": 0.85
-            }
-        });
+      ]);
+      const data = fireData;
+      let region = data[0].data;
+      region.forEach(reg => {
+        this.station.push(reg.name);
+      })
+
+      // data.forEach(bush => {
+      //   var marker = new mapboxgl.Marker()
+      //   .setLngLat([bush.xi, bush.yi])
+      //   .addTo(this.map);
+      // });
     });
   };
-  
+
   select(event) {
     this.selectSubstrate = event.target.value;
     this.map.setStyle(this.style + this.selectSubstrate);
-    setTimeout(() =>{
-    this.map.addSource("myImageSource", {
-      "type": "image",
-      "url": "../../assets/zem.png",
-      "coordinates": [
-          [74.88, 57.5],
-          [86.18, 57.5],
-          [86.18, 52.8],
-          [74.88, 52.8]
-      ]
-    });
-    this.map.addLayer({
-        "id": "overlay",
-        "source": "myImageSource",
-        "type": "raster",
-        "paint": {
-        "raster-opacity": 0.85
-        }
-    });}, 1000)
   };
+  selectStation(event) {
+
+    let select = event.target.value;
+    const data = fireData;
+    let region = data[0].data;
+    this.markers.forEach(m => {
+      m.remove();
+    })
+    this.markers =[];
+    region.forEach(reg => {
+      if (reg.name === select) {
+        reg.state.forEach(s => {
+          this.markers.push(
+            new mapboxgl.Marker()
+              .setLngLat([s.xi, s.yi])
+              .addTo(this.map));
+          this.markers[this.markers.length-1].getElement().addEventListener('click', () => {
+            alert("Clicked");
+          });
+        })
+      }
+    })
+  }
 
 }
